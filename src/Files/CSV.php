8a5e74cc827a45f834fc95410ca85b52bc5cbf36
Max Almonte <maxalmonte14@hotmail.com>
2018-10-12 15:14:22 -0400

    Moved to Files domain

---
+++ src/Files/CSV.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace MidasSoft\DominicanBankParser\Files;
+
+class CSV extends AbstractFile
+{
+    /**
+     * Eliminates all unnecesary
+     * values in a CSV file.
+     *
+     * @return array
+     */
+    private function sanitize()
+    {
+        $fileWithoutEmptyLine = preg_replace('/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/', '\n', $this->fileContent);
+        $cleanedCsvAmounts = preg_replace('/,(?=[^\"]*\"[^\"]*(?:\"[^\"]*\"[^\"]*)*$)/', '', $fileWithoutEmptyLine);
+        $cleanedCsvAmounts = preg_replace('/[\"]+/', '', $cleanedCsvAmounts);
+
+        $lines = explode(PHP_EOL, $cleanedCsvAmounts);
+
+        return $lines;
+    }
+
+    /**
+     * Converts a CSV string to array.
+     *
+     * @param string $csvString
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        $csvArray = [];
+        foreach ($this->sanitize() as $key => $line) {
+            $actualLine = str_getcsv(strip_tags(stripslashes(trim($line))));
+            $csvArray[] = array_filter($actualLine);
+        }
+
+        return array_values(array_filter($csvArray));
+    }
+}
