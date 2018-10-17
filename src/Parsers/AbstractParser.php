c8ab361f3f6c2b1bd24c2d34606ad46c85cffd4e
Max Almonte <maxalmonte14@hotmail.com>
2018-10-17 16:29:43 -0400

    Added cache manager object

--- src/Parsers/AbstractParser.php
+++ src/Parsers/AbstractParser.php
@@ -5,9 +5,17 @@ namespace MidasSoft\DominicanBankParser\Parsers;
 use Illuminate\Support\Collection;
 use MidasSoft\DominicanBankParser\Exceptions\EmptyFileException;
 use MidasSoft\DominicanBankParser\Exceptions\InvalidArgumentException;
+use MidasSoft\DominicanBankParser\Interfaces\CacheInterface;
 
 class AbstractParser
 {
+    /**
+     * CacheInterface instance.
+     *
+     * @var \MidasSoft\DominicanBankParser\Interfaces\CacheInterface
+     */
+    protected $cacheManager = null;
+
     /**
      * Validates that a file has data to parse.
      *
@@ -22,5 +30,27 @@ class AbstractParser
         if (count($fileData) == 0) {
             throw new EmptyFileException('You\'re trying to parse an empty file.');
         }
+    }
+
+    /**
+     * Returns the cacheManager property.
+     *
+     * @return \MidasSoft\DominicanBankParser\Interfaces\CacheInterface
+     */
+    public function getCacheManager()
+    {
+        return $this->cacheManager;
+    }
+
+    /**
+     * Returns the cacheManager property.
+     *
+     * @param \MidasSoft\DominicanBankParser\Interfaces\CacheInterface
+     *
+     * @return void
+     */
+    public function setCacheManager(CacheInterface $cacheManager)
+    {
+        $this->cacheManager = $cacheManager;
     }
 }
