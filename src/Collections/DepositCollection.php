9e35712dbc3273003de4b6bb03dc9e905e9abbce
Max Almonte <maxalmonte14@hotmail.com>
2018-10-23 17:03:50 -0400

    Added DepositCollection for wrapping Deposit objects

---
+++ src/Collections/DepositCollection.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MidasSoft\DominicanBankParser\Collections;
+
+use MidasSoft\DominicanBankParser\Deposit;
+use MidasSoft\DominicanBankParser\Exceptions\InvalidArgumentException;
+use Tightenco\Collect\Support\Collection;
+
+class DepositCollection extends Collection
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct($items = [])
+    {
+        array_walk($items, function ($value, $key) {
+            if (!$value instanceof Deposit) {
+                throw new InvalidArgumentException('You should pass only Deposit objects to this collection.');
+            }
+        });
+
+        parent::__construct($items);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function push($value)
+    {
+        if (!$value instanceof Deposit) {
+            throw new InvalidArgumentException('You should pass only Deposit objects to this collection.');
+        }
+
+        parent::push($value);
+    }
+}
